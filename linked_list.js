f8d0c5fe8b1f0636336f3a1f0e9e820eea307728
Abdul Shabbir <abdulshabbir@Abduls-MacBook-Air.local>
2018-08-12 22:59:38 +0800

    clean up linked list constructor function

--- linked_list.js
+++ linked_list.js
@@ -1,45 +1,24 @@
 //----------------------CREATE A LINKED LIST ----------------------------//
 
 function LinkedList() {
-    this.head = {
-        next: null,
-    }
-    this.length = 1;
-}
-
-function Node(value) {
-    this.value = value;
-    this.next = null; 
+    this.head = null;
+    this.length = 0;
 }
 
 LinkedList.prototype.addToHead = function(value) {
-    //create a new node and assign value
-    newNode = new Node(value); 
+    //define a node to have a value and next pointer/reference
+    const newNode = {
+        value: value, 
+        next: null
+    };
 
-    //store head in a temporary variable
-    let tempHead = this.head; 
+    //make the next pointer of new node point towards where the head is currently pointing
+    newNode.next = this.head; 
 
-    if(tempHead.next === null) {
-        //if head doesn't reference anything, reference the new node
-        this.head.next = newNode;
-        this.length++;
-        console.log(tempHead.value);
-        return; 
-    }
+    //make the head point to the new node
+    this.head = newNode;
 
-    while(tempHead.next !== null) {
-        //traverse the head pointer to next node 
-        tempHead = tempHead.next; 
-    }
-
-    // set the next pointer of last node to point to the new node
-    tempHead.next = newNode;
     this.length++; 
-    console.log(tempHead.value);
+    console.log(this); 
+    return this; 
 }; 
-
-let list = new LinkedList();  
-list.addToHead('hi');
-list.addToHead('hi again');
-
-console.log(list);
